3593d8d79a6b746f543973c978b5fc9683959591
yessine <yessine.agrebi.aiesec@gmail.com>
2023-07-29 14:39:50 +0200

    auth router register

---
+++ api/src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { authController } from '../controllers/auth.controller';
+
+const router = express.Router()
+
+
+router.post('/register', authController.register)
+
+
+
+export default router
