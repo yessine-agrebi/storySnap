1e36083faabb1e873584e39b413a31f4bf274c58
yessine <yessine.agrebi.aiesec@gmail.com>
2023-07-30 02:16:05 +0200

    user routes

---
+++ api/src/routes/user.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  getAllUsersController,
+  getUserByIdController,
+  updateUserByIdController,
+  deleteUserByIdController,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+// Route for fetching all users
+router.get("/", getAllUsersController);
+
+// Route for fetching a single user by their ID
+router.get("/:userId", getUserByIdController);
+
+// Route for updating a user by their ID
+router.put("/:userId", updateUserByIdController);
+
+// Route for deleting a user by their ID
+router.delete("/:userId", deleteUserByIdController);
+
+export default router;
