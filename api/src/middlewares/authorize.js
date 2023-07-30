f754a452ac0a17b7996294e7c5059deb12db4fa3
yessine <yessine.agrebi.aiesec@gmail.com>
2023-07-30 13:03:19 +0200

    authorization middleware

---
+++ api/src/middlewares/authorize.js
@@ -0,0 +1,11 @@
+// middleware/authorize.js
+export const authorize = (roles) => (req, res, next) => {
+    const { role } = req.user;
+  
+    if (!roles.includes(role)) {
+      return res.status(403).json({ message: "Forbidden - You don't have permission to access this resource." });
+    }
+  
+    next();
+  };
+  
