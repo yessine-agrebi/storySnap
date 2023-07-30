8766c209e73ad394cb9d389b952814e1ecdbcbc1
yessine <yessine.agrebi.aiesec@gmail.com>
2023-07-30 14:01:47 +0200

    notification model

---
+++ api/src/models/notification.model.js
@@ -0,0 +1,31 @@
+// models/notification.model.js
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema({
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['comment', 'vote'],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+export default Notification;
